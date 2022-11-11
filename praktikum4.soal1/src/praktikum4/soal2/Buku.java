49439ded9f00ce0df6990117727136b0bc493264
Dessy Fitriaya Anwari <103984794+dessyfitriaya@users.noreply.github.com>
2022-11-11 10:13:58 +0800

    Delete praktikum4.soal1 directory

--- praktikum4.soal1/src/praktikum4/soal2/Buku.java
+++
@@ -1,8 +0,0 @@
-package praktikum4.soal2;
-public abstract class Buku {
-    protected String judul;
-    protected String penulis;
-    protected Integer tahun;
-    
-    protected void display(){};
-}
